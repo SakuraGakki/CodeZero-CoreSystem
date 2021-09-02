e5856f27c83ad4fa1a992d09c280fb57d97754e4
CodeZero <739230137@qq.com>
2021-09-02 13:51:04 +0800

    同一访问后段网关地址

--- CZ-CS-Web/src/config/config.js
+++ CZ-CS-Web/src/config/config.js
@@ -1,6 +1,6 @@
 export const BaseUrl = {
-  // ip:"http://59.110.218.235:8080", //正式环境
-  ip:"http://localhost:8000", //本地环境
+  ip:"http://59.110.218.235:8800/website", //正式环境
+  // ip:"http://localhost:8000", //本地环境
   getVisitNum:"/visitor/getVisitNum",  //获取用户数量
   uptVisitNum:"/visitor/uptVisitNum",  //更新访问量信息
 }
